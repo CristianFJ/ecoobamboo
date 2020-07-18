03f70970a607d9d3bb93f8c83c95b3fc4c7d6d09
deasdaniel <67293677+deasdaniel@users.noreply.github.com>
2020-07-18 00:43:49 -0500

    Add files via upload
    conexion para inicio de sesion

---
+++ conexion2.php
@@ -0,0 +1,18 @@
+<?php
+function conexionSQL(){
+
+    $link = new mysqli("localhost","root","","bd");
+    if($link->connect_error){
+        $error="Error de conexion:".$link->connect_error."Mensaje: ".$link->connect_error;
+        die($error);
+    }else{
+    
+        $q= "SET CHARACTER SET UTF8";
+    
+        return $link;
+      
+     }
+    }
+    
+
+?>
